806545182ec3ca2c94804b3d51b1b717fa955e5c
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:50:18 +0530

    Parameterized Constructor

--- Shapes/Shape.java
+++ Shapes/Shape.java
@@ -10,4 +10,11 @@ abstract class Shape {
     public Shape() {
         dim_one = dim_two = dim_three = 0;
     }
+
+    // Parameterized constructor
+    public Shape(double dim_one, double dim_two, double dim_three) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+    }
 }
