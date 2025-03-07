f375792581d605cace6b938836df092e9e3507b6
Ajit Jain <jainajit787@gmail.com>
2025-03-07 12:23:13 +0530

    Switch Case ( Rectangle, Square, Circle, Sphere, Cylinder, Pyramid, Exit , Default)

--- Shapes/Main.java
+++ Shapes/Main.java
@@ -16,6 +16,80 @@ public class Main {
             System.out.println("7. Exit");
 
             int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1: // Rectangle
+                    System.out.print("Enter length: ");
+                    double length = scanner.nextDouble();
+                    System.out.print("Enter width: ");
+                    double width = scanner.nextDouble();
+
+                    Rectangle rectangle = new Rectangle(length, width);
+                    System.out.println("Area: " + rectangle.calculateArea());
+                    System.out.println("Perimeter: " + rectangle.calculatePerimeter());
+                    break;
+
+                case 2: // Square
+                    System.out.print("Enter side length: ");
+                    double side = scanner.nextDouble();
+
+                    Square square = new Square(side);
+                    System.out.println("Area: " + square.calculateArea());
+                    System.out.println("Perimeter: " + square.calculatePerimeter());
+                    break;
+
+                case 3: // Circle
+                    System.out.print("Enter radius: ");
+                    double radius = scanner.nextDouble();
+
+                    Circle circle = new Circle(radius);
+                    System.out.println("Area: " + circle.calculateArea());
+                    System.out.println("Perimeter (Circumference): " + circle.calculatePerimeter());
+                    break;
+
+                case 4: // Sphere
+                    System.out.print("Enter radius: ");
+                    double sphereRadius = scanner.nextDouble();
+
+                    Sphere sphere = new Sphere(sphereRadius);
+                    System.out.println("Surface Area: " + sphere.calculateArea());
+                    System.out.println("Volume: " + sphere.calculateVolume());
+                    break;
+
+                case 5: // Cylinder
+                    System.out.print("Enter radius: ");
+                    double cylinderRadius = scanner.nextDouble();
+                    System.out.print("Enter height: ");
+                    double cylinderHeight = scanner.nextDouble();
+
+                    Cylinder cylinder = new Cylinder(cylinderRadius, cylinderHeight);
+                    System.out.println("Surface Area: " + cylinder.calculateArea());
+                    System.out.println("Volume: " + cylinder.calculateVolume());
+                    break;
+
+                case 6: // Pyramid
+                    System.out.print("Enter base area: ");
+                    double baseArea = scanner.nextDouble();
+                    System.out.print("Enter slant height: ");
+                    double slantHeight = scanner.nextDouble();
+                    System.out.print("Enter height: ");
+                    double height = scanner.nextDouble();
+
+                    Pyramid pyramid = new Pyramid(baseArea, slantHeight, height);
+                    System.out.println("Surface Area: " + pyramid.calculateArea());
+                    System.out.println("Perimeter: " + pyramid.calculatePerimeter());
+                    System.out.println("Volume: " + pyramid.calculateVolume());
+                    break;
+
+                case 7: // Exit
+                    exit = true;
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
         }
     }
 }
