059a6c972a5a0965f03ae3b41eca031d63f33145
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:54:10 +0530

    Overridden Method

--- Shapes/Square.java
+++ Shapes/Square.java
@@ -2,4 +2,14 @@ class Square extends Shape {
     public Square(double side) {
         super(side, side, 0);
     }
+
+    @Override
+    public double calculateArea() {
+        return dim_one * dim_one;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * dim_one;
+    }
 }
