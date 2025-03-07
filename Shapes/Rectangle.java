cfa477a5cc3fa864f45fb137fcc2d4f03f1b6388
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:43:54 +0530

    Overridden Method

--- Shapes/Rectangle.java
+++ Shapes/Rectangle.java
@@ -2,4 +2,14 @@ class Rectangle extends Shape {
     public Rectangle(double length, double width) {
         super(length, width, 0);
     }
+
+    @Override
+    public double calculateArea() {
+        return dim_one * dim_two;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (dim_one + dim_two);
+    }
 }
