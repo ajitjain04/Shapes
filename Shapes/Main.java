ad822f62e77caf48bb7ce0c41b2bbf8d2c0b145c
Ajit Jain <jainajit787@gmail.com>
2025-03-07 12:20:49 +0530

    Menu Options

--- Shapes/Main.java
+++ Shapes/Main.java
@@ -6,6 +6,14 @@ public class Main {
         boolean exit = false;
 
         while (!exit) {
+            System.out.println("Choose a shape to calculate properties:");
+            System.out.println("1. Rectangle");
+            System.out.println("2. Square");
+            System.out.println("3. Circle");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Pyramid");
+            System.out.println("7. Exit");
         }
     }
 }
