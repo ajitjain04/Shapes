3977e3a7a0e04ef0ffdf229f95cabb2954fc832a
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:31:07 +0530

    Overridden Methods

--- Shapes/Cylinder.java
+++ Shapes/Cylinder.java
@@ -3,4 +3,19 @@ class Cylinder extends Shape implements Volume {
     public Cylinder(double radius, double height) {
         super(radius, 0, height);
     }
+
+    @Override
+    public double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + dim_three);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * dim_one;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return Math.PI * dim_one * dim_one * dim_three;
+    }
 }
