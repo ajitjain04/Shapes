4954d7ddcb94283a94eb583aa5634240444fa689
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:32:47 +0530

    Constructor

--- Shapes/Pyramid.java
+++ Shapes/Pyramid.java
@@ -1,4 +1,8 @@
 //Pyramid.java
 
 class Pyramid extends Shape {
+    public Pyramid(double baseArea, double slantHeight, double height) {
+        // Use the three-parameter constructor of Shape
+        super(baseArea, slantHeight, height);
+    }
 }
