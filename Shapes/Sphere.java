0d00666922755013c3b0e74ba678fe3aea3c4599
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:53:04 +0530

    Overridden Method

--- Shapes/Sphere.java
+++ Shapes/Sphere.java
@@ -3,4 +3,19 @@ class Sphere extends Shape implements Volume {
     public Sphere(double radius) {
         super(radius, 0, 0);
     }
+
+    @Override
+    public double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 0; // Perimeter is not applicable to a sphere
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3) * Math.PI * Math.pow(dim_one, 3);
+    }
 }
