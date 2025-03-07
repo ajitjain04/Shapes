c3bc36d489d578cd20bd523b026ea1c25185feb6
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:29:44 +0530

    Overridden Method

--- Shapes/Circle.java
+++ Shapes/Circle.java
@@ -5,4 +5,14 @@ class Circle extends Shape {
     public Circle(double radius) {
         super(radius, 0, 0);
     }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * dim_one * dim_one;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * dim_one;
+    }
 }
