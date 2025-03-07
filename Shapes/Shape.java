455248d222e455542df0ecf48741fba3440334f4
Ajit Jain <jainajit787@gmail.com>
2025-03-07 11:49:36 +0530

    Default Constructor

--- Shapes/Shape.java
+++ Shapes/Shape.java
@@ -5,4 +5,9 @@ abstract class Shape {
     protected double dim_one;
     protected double dim_two;
     protected double dim_three;
+
+    // Default constructor
+    public Shape() {
+        dim_one = dim_two = dim_three = 0;
+    }
 }
